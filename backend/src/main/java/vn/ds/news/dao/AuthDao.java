0c9f10ba557fc467ac6368b1a7704f0a735e0ec9
diepviends <giusediep@gmail.com>
2017-06-13 15:40:12 +0700

    remove

--- backend/src/main/java/vn/ds/news/dao/AuthDao.java
+++
@@ -1,7 +0,0 @@
-package vn.ds.news.dao;
-
-public interface AuthDao {
-	public boolean isUserValid(String username, String password);
-
-	public boolean isGranted(String username, String path, String method);
-}
